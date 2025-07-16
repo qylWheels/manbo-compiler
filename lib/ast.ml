484d9b34f22150a9c417e937d197a16e1d535c8b
qylWheels <command1748165360@126.com>
2025-07-16 19:59:19 +0800

    完成对ast的定义

---
+++ lib/ast.ml
@@ -0,0 +1,52 @@
+(* 关键字 *)
+type keyword =
+  Var | Const | Fn | If | While
+
+(* 一元运算符 *)
+type unaryop =
+  Plus | Minus
+
+(* 二元运算符 *)
+and binop =
+  Add | Sub | Mul | Div | Mod (* 算术运算符 *)
+  | Less | Le | Equal | Ge | Greater | NotEq (* 比较运算符 *)
+  | LogicalAnd | LogicalOr | LogicalNot (* 逻辑运算符 *)
+  | Assign (* 赋值运算符 *)
+
+(* 表达式 *)
+type expr =
+  Unit
+  | Number of float
+  | String of string
+  | Identifier
+  | UnaryExpr of unaryop * expr
+  | BinExpr of expr * binop * expr
+  | FnExpr of {
+      arguments: (expr * type_annotation) list;
+      return: type_annotation;
+      statements: statement list;
+      tail_expr: expr;
+    }
+  | IfExpr of {
+      guard: expr;
+      then_branch: statement list;
+      then_tail_expr: expr;
+      else_branch: statement list;
+      else_tail_expr: expr;
+    }
+
+(* 类型注解 *)
+and type_annotation =
+  Unit | Number | String
+
+(* 语句 *)
+and statement =
+  AssignStmt of {
+    qualifier: [`Var | `Const];
+    lhs: expr;
+    rhs: expr;
+  }
+  | WhileStmt of {
+      condition: expr;
+      statements: statement list;
+    }
